47d99b6c2a0db751118a2025ff3584ea88075dca
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2017-11-09 11:02:10 +0000

    clion stuff

---
+++ lab2/cmake-build-debug/CMakeFiles/lab2.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gpereira/Documents/FEUP/3/SBMI/Works/LAB2/lab2/src/main.cpp" "/home/gpereira/Documents/FEUP/3/SBMI/Works/LAB2/lab2/cmake-build-debug/CMakeFiles/lab2.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ARDUINO=10619"
+  "ARDUINO_ARCH_AVR"
+  "ARDUINO_AVR_UNO"
+  "F_CPU=16000000L"
+  "PLATFORMIO=30401"
+  "__AVR_ATmega328P__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/cores/arduino"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/variants/standard"
+  "../src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/EEPROM/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/HID/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/SPI/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/SoftwareSerial/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/Wire/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Adafruit_CircuitPlayground"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Adafruit_CircuitPlayground/utility"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Bridge/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Esplora/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Ethernet/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Firmata"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Firmata/utility"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/GSM/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Keyboard/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/LiquidCrystal/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Mouse/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/RobotIRremote/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Robot_Control/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Robot_Motor/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/SD/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Servo/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/SpacebrewYun/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Stepper/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/TFT/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/Temboo/src"
+  "/home/gpereira/.platformio/packages/framework-arduinoavr/libraries/WiFi/src"
+  "/home/gpereira/.platformio/packages/toolchain-atmelavr/avr/include"
+  "/home/gpereira/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/4.9.2/include"
+  "/home/gpereira/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/4.9.2/include-fixed"
+  "/home/gpereira/.platformio/packages/tool-unity"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
