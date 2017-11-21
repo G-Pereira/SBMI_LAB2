cd6ab3f3434c46e6dd1ebc226daa7847a1691453
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2017-11-21 20:07:34 +0000

    Clean Code

--- lab2/src/main.c
+++ lab2/src/main.c
@@ -6,7 +6,7 @@
 
 #define RED PB0
 #define GREEN PB1
-#define BUTTON PB2
+#define BUTTON PD2
 
 #define COUNT (256-250)
 
@@ -14,41 +14,40 @@
 #define F_CPU 16000000ul
 #endif
 #define BAUD 57600
-#define UBBR_VAL ((F_CPU/(BAUD*16))-1)
+#define UBBR_VAL ((F_CPU/(BAUD*16))-1) // Calculate UBBR value based on desired baudrate
 
 uint8_t state = 0;
 uint16_t volatile timer = 0;
 
-uint8_t EEMEM signature = 0b11110100; // TODO: Why initialize?
-uint16_t EEMEM best = UINT16_MAX;
+uint8_t EEMEM signature;
+uint16_t EEMEM best;
 
-ISR(TIMER2_OVF_vect) {
+ISR (TIMER2_OVF_vect) {
     TCNT2 = COUNT;
-    if (timer)
+    if (timer > 0)
         timer--;
 }
 
 int main() {
-    TCCR2A = 0;
-    TCCR2B = 0;
-    TCCR2B |= (1 << CS22);
-    TIMSK2 |= (1 << TOIE2);
-    TCNT2 = COUNT;
+    TCCR2A = 0; // Normal Mode
+    TCCR2B = (1 << CS22); // /64 prescaler
+    TIMSK2 |= (1 << TOIE2); // Enable Overflow Interrupt
+    TCNT2 = COUNT; // set base time to make the timer overflow match 1ms time base
 
     DDRB |= (1 << GREEN);
     DDRB |= (1 << RED);
 
-    DDRB &= ~(1 << BUTTON);
-    PORTB |= (1 << BUTTON);
+    DDRD &= ~(1 << BUTTON);
+    PORTD |= (1 << BUTTON); // Internal Pull-up
 
     UBRR0 = UBBR_VAL;
-    UCSR0B = (1 << RXEN0 | 1 << TXEN0);
-    UCSR0C = (1 << USBS0 | 3 << UCSZ00);
+    UCSR0B = 1 << TXEN0; // Enable Transmitter (TX)
+    UCSR0C = 3 << UCSZ00; // Data frame length
 
     sei();
 
     // Check if memory is signed to initialize score or not
-    if (eeprom_read_byte(&signature) != 0b11110100 || !(PINB & (1 << BUTTON))) {
+    if (eeprom_read_byte(&signature) != 0b11110100 || !(PIND & (1 << BUTTON))) {
         eeprom_write_byte(&signature, 0b11110100);
         eeprom_write_word(&best, UINT16_MAX);
     }
@@ -61,19 +60,19 @@ int main() {
     init_printf_tools();
 
     while (1) {
-        if (0 == state && (!(PINB & (1 << BUTTON)))) {
+        if (0 == state && (!(PIND & (1 << BUTTON)))) {
             srand(TCNT2);
             timer = (uint16_t) rand() % 5000 + 5000;
             state = 1;
-        }else if(1==state && !(PINB & (1 << BUTTON))){
+        } else if (1 == state && !(PINB & (1 << BUTTON))) {
             printf("Attempt number %u= %ums due to faulty play! You bastard...\n", attemptN + 1, 5000);
             sum += 5000;
             attemptN++;
-            state = 0;
+            state = 3;
         } else if (1 == state && 0 == timer) {
             state = 2;
             timer = UINT16_MAX;
-        } else if (2 == state && !(PINB & (1 << BUTTON))) {
+        } else if (2 == state && !(PIND & (1 << BUTTON))) {
             // value to store UINT16_MAX - timer
             currentAttempt = UINT16_MAX - timer;
             printf("Attempt number %u= %ums\n", attemptN + 1, currentAttempt);
@@ -85,11 +84,11 @@ int main() {
             sum += 5000;
             attemptN++;
             state = 0;
-        } else if (3 == state && (PINB & (1 << BUTTON))) {
+        } else if (3 == state && (PIND & (1 << BUTTON))) {
             state = 0;
         }
 
-        if (0 == state) {
+        if ((0 == state) || (3 == state)) {
             PORTB &= ~(1 << GREEN);
             PORTB &= ~(1 << RED);
         } else if (1 == state) {
@@ -98,9 +97,6 @@ int main() {
         } else if (2 == state) {
             PORTB |= (1 << GREEN);
             PORTB |= (1 << RED);
-        } else if (3 == state) {
-            PORTB &= ~(1 << GREEN);
-            PORTB &= ~(1 << RED);
         }
 
         if (5 == attemptN) {
